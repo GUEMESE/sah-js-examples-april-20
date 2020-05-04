861dcdabbee50287c5b77edac1d0f9b5f03beadb
GUEMESE <guldenemese86@gmail.com>
2020-05-04 18:43:34 +0200

    add well toggles

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-console.log('Script file is loaded.');
 
 // Third part:
 
-$('h1').text('This is done by JavaScript!');
+console.log('Script file is loaded.');
+
+$('h1').html('This is done by <em>JavaScript</em>!');
 $('p').css('border','1px solid red');
 $('p').css('padding', '10px');
-$('h2').toggleClass('blue')
+$('h2').toggleClass('blue');
 
 
 
